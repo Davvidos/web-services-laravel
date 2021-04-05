e80e4be6dd54aa5a3a235c20ca3c92d0fdce7e9c
Dawid Jurczyński <dawidjurczynski@gmail.com>
2021-04-05 13:46:30 +0200

    rest-api Create rest api example
    Remove some files from idea dir. Create new 404 json handler. Create
    article controller. Create default article model. Create migration for
    articles. Create default seeder with articles. Create routes for api
    endpoints.

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+declare(strict_types=1);
+
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('articles', [ArticleController::class, 'index']);
+Route::get('articles/{article}', [ArticleController::class, 'show']);
+Route::post('articles', [ArticleController::class, 'create']);
+Route::put('articles/{article}', [ArticleController::class, 'update']);
+Route::delete('articles/{article}', [ArticleController::class, 'delete']);
